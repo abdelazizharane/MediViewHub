ffccd273f6491371554230f32d61e0db413c80e0
Abdel-aziz Harane <122400488+AbdoulazizH@users.noreply.github.com>
2023-08-06 22:40:27 +0100

    Delete mediviewhub directory

--- mediviewhub/scripts_php/check_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Vérifier si l'utilisateur est connecté en vérifiant si la variable de session "username" est définie
-if (!isset($_SESSION['username'])) {
-    // Rediriger l'utilisateur vers la page de connexion s'il n'est pas connecté
-    header("Location: login.html");
-    exit();
-}
-?>
